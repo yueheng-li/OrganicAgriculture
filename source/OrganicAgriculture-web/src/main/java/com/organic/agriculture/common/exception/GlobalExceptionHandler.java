0f76daa6e14114952bd35a367e3bf763db4caaa6
Li <chunhui.li@accenture.com>
2017-06-12 16:24:58 +0800

    delete 不要的import

--- source/OrganicAgriculture-web/src/main/java/com/organic/agriculture/common/exception/GlobalExceptionHandler.java
+++ source/OrganicAgriculture-web/src/main/java/com/organic/agriculture/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,10 @@
 package com.organic.agriculture.common.exception;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice

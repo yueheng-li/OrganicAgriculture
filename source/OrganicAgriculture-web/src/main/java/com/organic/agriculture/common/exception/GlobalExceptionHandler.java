e612b33f2e76b38cba939a73ee7f4454e5d4aaee
Li <chunhui.li@accenture.com>
2017-06-12 16:12:12 +0800

    拦截异常方法添加。

--- source/OrganicAgriculture-web/src/main/java/com/organic/agriculture/common/exception/GlobalExceptionHandler.java
+++ source/OrganicAgriculture-web/src/main/java/com/organic/agriculture/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.organic.agriculture.common.exception;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
@@ -31,4 +35,26 @@ public class GlobalExceptionHandler {
         mav.setViewName(DEFAULT_ERROR_VIEW);
         return mav;
     }
+    
+
+    /**
+     * session失效的异常拦截
+     * 把Exception替换成session time out的exception，shiro的时候使用InvalidSessionException，UnknownSessionException
+     */
+//    @ExceptionHandler(Exception.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public String sessionTimeout(Exception e, Model model, HttpServletRequest request, HttpServletResponse response) {
+//        model.addAttribute("tips", "session超时");
+//        return "/login.html";
+//    }
+    
+    /**
+     * 拦截未知的运行时异常
+     *
+     */
+//    @ExceptionHandler(RuntimeException.class)
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    @ResponseBody
+//    public ErrorInfo notFount(RuntimeException e) {
+//    }
 }

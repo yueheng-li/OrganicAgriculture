3410f4701228d7416de9858b6eec1d563e1f4b25
Li <chunhui.li@accenture.com>
2017-06-13 16:06:59 +0800

    api 网管追加

--- source/OrganicAgriculture-LoadBalanced/src/main/java/com/organic/agriculture/LoadBalancedApplication.java
+++ source/OrganicAgriculture-LoadBalanced/src/main/java/com/organic/agriculture/LoadBalancedApplication.java
@@ -2,9 +2,16 @@ package com.organic.agriculture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+import com.organic.agriculture.filter.AccessFilter;
+
+@EnableZuulProxy
+@SpringCloudApplication
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
@@ -17,4 +24,8 @@ public class LoadBalancedApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoadBalancedApplication.class, args);
 	}
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 }

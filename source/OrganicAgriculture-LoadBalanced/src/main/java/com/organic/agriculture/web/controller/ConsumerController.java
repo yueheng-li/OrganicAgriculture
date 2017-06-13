2285cfe06315b8c9c87a65f7c32aab82a88fe83d
Li <chunhui.li@accenture.com>
2017-06-13 11:17:45 +0800

    使用Ribbon来调用服务，并实现客户端的均衡负载。

---
+++ source/OrganicAgriculture-LoadBalanced/src/main/java/com/organic/agriculture/web/controller/ConsumerController.java
@@ -0,0 +1,20 @@
+package com.organic.agriculture.web.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ConsumerController {
+	private final Logger logger = Logger.getLogger(getClass());
+	@Autowired
+	RestTemplate restTemplate;
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add() {
+		return restTemplate.getForEntity("http://COMPUTE-SERVICE/add?a=10&b=20", String.class).getBody();
+	}
+}

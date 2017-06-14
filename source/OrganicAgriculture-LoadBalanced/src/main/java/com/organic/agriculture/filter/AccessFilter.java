cd3c091a9887ced166ba159bffae9879826330de
Li <chunhui.li@accenture.com>
2017-06-14 15:00:30 +0800

    no message

--- source/OrganicAgriculture-LoadBalanced/src/main/java/com/organic/agriculture/filter/AccessFilter.java
+++ source/OrganicAgriculture-LoadBalanced/src/main/java/com/organic/agriculture/filter/AccessFilter.java
@@ -1,10 +1,13 @@
 package com.organic.agriculture.filter;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
+import com.google.common.io.CharStreams;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.organic.agriculture.web.client.ComputeClient;
@@ -34,10 +37,16 @@ public class AccessFilter extends ZuulFilter {
 	public Object run() {
 		RequestContext ctx = RequestContext.getCurrentContext();
 		HttpServletRequest request = ctx.getRequest();
-
 		Object accessToken = request.getParameter("accessToken");
 		
 		String token = request.getParameter("token");
+		if ("POST".equalsIgnoreCase(request.getMethod())) {
+			try {
+				token = CharStreams.toString(request.getReader());
+			} catch (IOException e) {
+				token = null;
+			}
+	    }
 		if (token != null && token.length() > 0) {
 			ResponseEntity<String> resp = computeClient.authenticate(token);
 		}
@@ -45,9 +54,9 @@ public class AccessFilter extends ZuulFilter {
 
 			ctx.setSendZuulResponse(false);
 			ctx.setResponseStatusCode(401);
-			return 10002;
+			return null;
 		}
-		return 200;
+		return null;
 	}
 
 }

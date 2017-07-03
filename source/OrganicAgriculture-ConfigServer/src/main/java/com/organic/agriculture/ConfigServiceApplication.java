189689fd8cb21106a43a709e476f78cdc8c217b6
Li <chunhui.li@accenture.com>
2017-07-03 08:06:19 +0800

    config service 配置文件管理服务

---
+++ source/OrganicAgriculture-ConfigServer/src/main/java/com/organic/agriculture/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.organic.agriculture;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConfigServiceApplication.class).web(true).run(args);
+	}
+}
